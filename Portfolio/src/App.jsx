c2786da87f3f2a48ea74f6ef08af3811d37214d0
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2023-04-03 13:51:06 -0400

    added threejs canvas

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
+import * as THREE from 'three'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect( () => {
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
+const renderer = new THREE.WebGLRenderer({canvas:document.querySelector('#mycanvas')})
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30)
+renderer.render(scene,camera)
+  })
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+     
+      <canvas id='mycanvas'/>
+    
+     
     </div>
   )
 }
