f111e1a915b47b5e733435fb2ac6ade8d37feebc
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2023-04-06 14:35:28 -0400

    added components and three js code

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -8,19 +8,21 @@ function App() {
   useEffect( () => {
     const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
-const renderer = new THREE.WebGLRenderer({canvas:document.querySelector('#mycanvas')})
+    const canvas = document.getElementById('mycanvas')
+const renderer = new THREE.WebGLRenderer({canvas, antialias:true})
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30)
 renderer.render(scene,camera)
+document.body.appendChild(renderer.domElement);
   })
 
 
   return (
     <div>
      
-      <canvas id='mycanvas'/>
+
     
      
     </div>
