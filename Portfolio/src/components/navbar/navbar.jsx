95ed0d9b8d069a1618300b2dd66cfc8b56d7a558
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2023-04-07 08:05:57 -0400

    added some code to navbar component and installed sass

--- Portfolio/src/components/navbar/navbar.jsx
+++ Portfolio/src/components/navbar/navbar.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
 import Social from '../nav/nav'
+import './navbar.scss'
 
 const Section = styled.div`
 height:10vh;
 scroll-snap-align: center ;
-background-color: grey;
+
 `
 
 function Navbar() {
   return (
     <div>
-      <Social/>
+   
         <Section>
-        <div> Navbar </div>
+        <header>
+          <nav>
+
+          </nav>
+          <h2> Henrich</h2>
+          <Social/>
+        </header>
 
 
         </Section>
