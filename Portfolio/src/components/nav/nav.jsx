3e871315d0e133359b2f5795ecb778ca4c531803
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2023-04-06 20:01:41 -0400

    updated the readme

--- Portfolio/src/components/nav/nav.jsx
+++ Portfolio/src/components/nav/nav.jsx
@@ -10,17 +10,17 @@ function Social() {
     <Nav defaultActiveKey="/home" as="ul">
     <Nav.Item as="li">
       <Nav.Link href="/home">  
-      <SocialIcon url='https://Github.com/Henrich77' network='github'/>
+      <SocialIcon url='https://Github.com/Henrich77' network='github' fgColor="#882525" bgColor='transparent' />
       </Nav.Link>
     </Nav.Item>
     <Nav.Item as="li">
       <Nav.Link href="https://www.linkedin.com/in/henrich-tanis-723a43172/">
-      <SocialIcon url='https://www.linkedin.com/in/henrich-tanis-723a43172/'/>
+      <SocialIcon url='https://www.linkedin.com/in/henrich-tanis-723a43172/' fgColor="#882525" bgColor='transparent'/>
       </Nav.Link>
     </Nav.Item>
     <Nav.Item as="li">
       <Nav.Link href="mailto:henrichtanis@gmail.com">
-      <SocialIcon url='mailto:henrichtanis@gmail.com' network='mailto'/>
+      <SocialIcon url='mailto:henrichtanis@gmail.com' network='mailto' fgColor="#882525" bgColor='transparent'/>
       </Nav.Link>
     </Nav.Item>
   </Nav>
