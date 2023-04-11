7ff3d38d5c6b8e0f3db9f9dea7d653c8aa1cd71e
Henrich Tanis <henrichtanis@gmail.com>
2023-04-11 18:23:08 -0400

    😒'

--- Portfolio/deploy.sh
+++ Portfolio/deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:Henrich77/Henrich77.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# git push -f git@github.com:Henrich77/poor-4-lio.git main:gh-pages
 
 cd -
