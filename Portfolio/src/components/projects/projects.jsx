8f16f146eb5b2791d94a53a3d2ff9f5f11bfc0d0
Henrich Tanis <henrichtanis@gmail.com>
2023-04-12 16:26:28 -0400

    created a constants component for projects, and added some code to projects component

--- Portfolio/src/components/projects/projects.jsx
+++ Portfolio/src/components/projects/projects.jsx
@@ -9,6 +9,31 @@ height:100vh;
 scroll-behavior : smooth;
 scroll-snap-align: center ;
 `
+const Card = (
+  title,
+  description,
+  source,
+  img,
+) =>{ 
+  return (
+    <div>
+
+        <div className="card">
+        <div>
+            <img className='img' src={img}/>
+        </div>
+        <div className="info">
+            <h2> {title}</h2>
+            <p>{description}</p>
+          <div></div>
+        </div>
+      </div>
+      
+    </div>
+  )
+
+
+}
 
 function Main() {
   return (
